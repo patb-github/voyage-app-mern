011e4793d8952fe19e1ec8ca355fe5c5abf668c1
SmileNattakit <nattakit.rattanakeha@gmail.com>
2024-06-11 10:17:22 +0700

    create voyage project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Layout from './components/Layout';
+const App = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
