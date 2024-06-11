cdeb94d3ceffdbf785595929366d2288b0a6bb11
maielfth <165782420+maielfth@users.noreply.github.com>
2024-06-11 15:29:46 +0700

    register and login pages

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,47 +1,116 @@
-import { Link } from 'react-router-dom';
-const Register = () => {
-  return (
-    <div className="h-full bg-cover bg-center bg-no-repeat bg-[url('/bg.svg')]">
-      <section>
-        <div className="h-full bg-cover bg-center bg-no-repeat bg-[url('../../assets/bg.svg')]">
-          <div className="min-h-screen flex flex-col items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-md w-96">
-              <div className="text-3xl font-bold text-blue-500 mb-8">
-                <p className="drop-shadow-xl">Enjoy the trip with Voyage</p>
-              </div>
-              <h1 className="text-2xl text-left text-gray-800 font-semibold mb-6">
-                New Account
-              </h1>
-              <div className="relative">
-                <input
-                  className="w-full p-2 rounded mt-2 outline-none"
-                  type="email"
-                  placeholder="Email"
-                />
-                <hr className="absolute bottom-0 z-50 w-full bg-red-500" />
-              </div>
-              <div className="relative">
-                <input
-                  className="w-full p-2 rounded mt-2 outline-none"
-                  type="text"
-                  placeholder="Username"
-                />
-                <hr className="absolute bottom-0 z-50 w-full bg-red-500" />
-              </div>
-              <div className="relative">
-                <input
-                  className="w-full p-2 rounded mt-2 outline-none"
-                  type="password"
-                  placeholder="Password"
-                />
-                <hr className="absolute bottom-0 z-50 w-full bg-red-500" />
-              </div>
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
-              <Link to="/">
-                <button className="w-full mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                  Sign Up
+const Register = () => {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <div>
+      <section className="Login">
+        <div className="h-[370px] md:h-[93vh] bg-center bg-cover md:bg-auto md:bg-no-repeat   bg-[url('/bg-desktop.png')]">
+          <div className="px-8 py-8 drop-shadow-xl md:hidden font-bold text-3xl text-white mb-8 ">
+            <p>Enjoy the trip </p>
+            <p>with Voyage</p>
+          </div>
+          <div className="min-h-[75vh] flex flex-col items-center justify-end md:justify-center">
+            <div className="bg-white min-h-[500px] px-16 py-10  rounded-t-3xl md:rounded-3xl shadow-md w-full  md:w-96">
+              <div className="text-3xl font-bold text-blue-500 md:mb-8">
+                <p className="drop-shadow-xl hidden md:block">
+                  Enjoy the trip with Voyage
+                </p>
+              </div>
+              <span className="text-3xl  text-gray-800 font-bold">
+                <p>New Acount</p>
+              </span>
+              <form
+                className="flex flex-col mt-4"
+                onSubmit={(e) => console.log(e)}
+              >
+                <span className="label-text">Email</span>
+                <label className="input input-bordered flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70"
+                  >
+                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                  </svg>
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Email"
+                    {...register('email')}
+                  />
+                </label>
+                <span className="label-text">Username</span>
+                <label className="input input-bordered flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                  </svg>
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Username"
+                    {...register('username')}
+                  />
+                </label>
+                <span className="label-text">Password</span>
+                <label className="input input-bordered flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <input
+                    type="password"
+                    className="grow"
+                    placeholder="Password"
+                    {...register('password')}
+                  />
+                </label>
+                <span className="label-text">Password Confirmation</span>
+                <label className="input input-bordered flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <input
+                    type="password"
+                    className="grow"
+                    placeholder="Password Confirmation"
+                    {...register('password')}
+                  />
+                </label>
+                <button
+                  type="submit"
+                  className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-2"
+                >
+                  Sign In
                 </button>
-              </Link>
+              </form>
             </div>
           </div>
         </div>
